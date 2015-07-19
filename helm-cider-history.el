0fe0502b2224c739656268ba1345ba30026c4e2b
Andreas "Kungi" Klein <git@kungi.org>
2015-07-19 09:37:07 +0200

    Add comments, copyright, license and remove unused code

--- helm-cider-history.el
+++ helm-cider-history.el
@@ -1,13 +1,38 @@
-;; helm-cider-history
+;;; helm-cider-history.el --- Show cider input history in helm
 
+;; Copyright (C) 2015 Andreas Klein
 
-(defvar helm-cider-history-candidates
-  nil)
+;; Author: Andreas Klein <git@kungi.org>
+;; URL: https://github.com/Kungi/helm-cider-history
+;; Created: 19.07.2015
+;; Keywords: convenience
+;; Version: 0.0.1
+;; Package-Requires: ((helm "1.4.0") (cider "0.9.0"))
 
-(defun some-action (arg)
-  (message-box "%s\n%s"
-               (helm-get-selection)
-               (helm-marked-candidates)))
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
+(require 'helm)
+
+(defvar helm-cider-history-candidates nil)
 
 (defun helm-cider-history ()
   (interactive)
@@ -23,3 +48,5 @@
         :resume 'noresume))
 
 (provide 'helm-cider-history)
+
+;;; helm-cider-history.el ends here
